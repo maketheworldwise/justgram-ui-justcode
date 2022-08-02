f90612dc4d4bf9361aab5284a11916717a9abdd7
Kevin Ahn <maketheworldwise@gmail.com>
2022-08-02 11:01:36 +0900

    :recycle: 로그인 UI 리팩토링
    - HTML 구조 수정
    - HTML 구조 수정에 따른 CSS 수정
    - HTML 구조에 따른 JS 수정
    - 공통 CSS (flex) 그룹화

--- js/login.js
+++ js/login.js
@@ -1,16 +1,14 @@
-let defaultLoginClassName = "login__content";
-
-let loginContent = document.getElementsByClassName(defaultLoginClassName);
-let id = loginContent[0];
-let pw = loginContent[1];
-let btn = loginContent[2];
+let loginInput = document.getElementsByClassName("login__input");
+let id = loginInput[0];
+let pw = loginInput[1];
+let btn = document.querySelector(".login__btn > button");
 
 document.addEventListener("keyup", function (event) {
   if (id.value.length > 0 && pw.value.length > 0) {
     btn.disabled = false;
-    btn.className = defaultLoginClassName + " login__btn-enable";
+    btn.className = "login__btn-enable";
   } else {
     btn.disabled = true;
-    btn.className = defaultLoginClassName + " login__btn-disable";
+    btn.className = "login__btn-disable";
   }
 });
